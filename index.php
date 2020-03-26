8723a3101fc0d417ff348b88a9ecb0dcd7b58851
gilbertsei <62713795+gilbertsei@users.noreply.github.com>
2020-03-27 01:15:06 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,26 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Taxi Online</title>
-</head>
-<body>
+<title>Taxi Online</title>
 <?php
-include 'database.php';
-$conn = OpenCon();
-echo "Connected Successfully";
-CloseCon($conn);
+required_once("database.php");
 ?>
+
+<body>
+	<h2>Driver</h2>
+<table> 
+	<tr>
+		<th>ID</th>
+		<th>Name</th>
+		<th>License</th>
+	</tr>
+</php
+	$sql = "SELECT * FROM driver ORDER by name ASC";
+	$result = $c->query($sql);
+	if ($result->num_rows > 0) {
+	$driver = array();
+	$i = 0;
+	while ($obj = $result->fetch_assoc()) {
+	echo '<tr>';
+	echo '<td>' . $obj['id']. '</td>';
+	echo '<td>' . $obj['name']. '</td>';
+	echo '<td>' . $obj['license']. '</td>';
+	}
 </body>
-</html>
