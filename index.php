389965d2f26ecf4dafb93db32c4fa37384ac4385
gilbertsei <62713795+gilbertsei@users.noreply.github.com>
2020-03-27 01:00:19 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,13 +4,11 @@
 	<title>Taxi Online</title>
 </head>
 <body>
-<?php $sql = "CREATE TABLE database.php (
-id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-firstname VARCHAR(30) NOT NULL,
-lastname VARCHAR(30) NOT NULL,
-email VARCHAR(50),
-reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-)";
+<?php
+include 'db_connection.php';
+$conn = OpenCon();
+echo "Connected Successfully";
+CloseCon($conn);
 ?>
 </body>
 </html>
